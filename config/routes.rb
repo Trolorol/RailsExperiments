ed0f51874d15a16d17ac89484411057f54b68c28
João Calapez <joaocalapez@pop-os.localdomain>
2020-02-03 17:42:45 +0000

    Finished tutorial 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'pages/info'
   resources :ideas
+  root to: redirect('/ideas')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
