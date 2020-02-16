9eab5c440a2727c42cee9e86f014dbd3c9b46909
João Calapez <joaocalapez@pop-os.localdomain>
2020-02-16 04:06:37 +0000

    Implemented devise. Login is now required to commit ideas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :request_is_json?
+  before_action :authenticate_user!
 
   def request_is_json?
     request.format.json?
